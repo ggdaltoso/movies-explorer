5ec85f2866f5fb17950ad925323d1ad0df3c9f00
ggdaltoso <ggdaltoso@gmail.com>
2024-09-06 09:32:01 -0300

    feat: tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ClippyProvider } from '@react95/clippy';
+import App from './App.tsx';
+
+import '@react95/core/GlobalStyle';
+import '@react95/core/themes/win95.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* <ClippyProvider>
+      <App />
+    </ClippyProvider> */}
   </StrictMode>,
-)
+);
